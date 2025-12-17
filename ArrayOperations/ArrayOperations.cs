89ac14c8db44c932342a04a19dc2a00e6fb81a1a
Irshad Shaikh <irshad.shaikh1992@gmail.com>
2025-12-17 18:03:42 +0530

    Array Basic Operations

--- ArrayOperations/ArrayOperations.cs
+++ ArrayOperations/ArrayOperations.cs
@@ -4,11 +4,37 @@ using System.Text;
 
 namespace ArrayOperations
 {
-    internal class ArrayOperations
+    public static class ArrayOperations
     {
         public static void BasicArrayOperations()
         {
-            
+            int[] num = new int[5];
+            int[] initializedArray = { 1, 2, 3, 4, 5 };
+            int[] sizedArray = new int[3] { 10, 20, 30 };
+
+            Console.WriteLine("Basic Array Operations:");
+            Console.WriteLine("Initialized Array: " + string.Join(", ", initializedArray));
+            Console.WriteLine("Sized Array: " + string.Join(", ", sizedArray));
+
+            //Access & Modify Elements
+            num[0] = 100;
+            Console.WriteLine("Modified First Element of num: " + num[0]);
+            int value = num[0];
+            Console.WriteLine("Accessed First Element of num: " + value);
+
+            //Array Properties - Length & Rank
+            Console.WriteLine($"Length: {num.Length}");
+            Console.WriteLine($"Rank: {num.Rank}");
+
+
+            // Example implementation
+            Console.WriteLine("Iterating over initializedArray:");  
+            foreach (var number in initializedArray)
+            {
+                System.Console.WriteLine(number);
+            }
+
+           
         }
     }
 }
